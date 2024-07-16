319bd74b0f8190d899bf24ec7871dd297260e891
mireal <moveloader@yandex.ru>
2024-07-16 13:40:16 +0300

    Added static method getDirectionalPointer to Pointer2D class

--- src/main/java/ru/snake/game/Pointer2D/Pointer2D.java
+++ src/main/java/ru/snake/game/Pointer2D/Pointer2D.java
@@ -1,12 +1,32 @@
 package ru.snake.game.Pointer2D;
 
-public record Pointer2D(int x,int y) {
+import ru.snake.game.Directions.Directions;
+
+public record Pointer2D(int x, int y) {
     public Pointer2D add(Pointer2D pointer2D) {
         return new Pointer2D(x + pointer2D.x(),y + pointer2D.y());
     }
 
     public Pointer2D sub(Pointer2D pointer2D) {
         return new Pointer2D(x - pointer2D.x(),y - pointer2D.y());
+    }
+
+    public static Pointer2D getDirectionalPointer(Directions direction) {
+        switch (direction) {
+            case UP -> {
+                return new Pointer2D(0,1);
+            }
+            case DOWN -> {
+                return new Pointer2D(0,-1);
+            }
+            case LEFT -> {
+                return new Pointer2D(-1,0);
+            }
+            case RIGHT -> {
+                return new Pointer2D(1,0);
+            }
+            default -> throw new IllegalStateException("Unexpected value: " + direction);
+        }
     }
 
     @Override
--- src/test/java/ru/snake/game/Pointer2D/Pointer2DTest.java
+++ src/test/java/ru/snake/game/Pointer2D/Pointer2DTest.java
@@ -1,18 +1,36 @@
 package ru.snake.game.Pointer2D;
 
 import org.junit.jupiter.api.Test;
+import ru.snake.game.Directions.Directions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class Pointer2DTest {
     @Test
-    void addMethodTest() {
+    void add_Method_Test() {
         Pointer2D pointer1 = new Pointer2D(1,1);
         Pointer2D pointer2 = new Pointer2D(2,2);
 
         Pointer2D additionResult = pointer1.add(pointer2);
         Pointer2D result = new Pointer2D(3,3);
         assertEquals(result,additionResult);
+    }
+
+    @Test
+    void sub_Method_Test() {
+        Pointer2D pointer1 = new Pointer2D(1,1);
+        Pointer2D pointer2 = new Pointer2D(1,1);
+
+        Pointer2D additionResult = pointer1.sub(pointer2);
+        Pointer2D result = new Pointer2D(0,0);
+        assertEquals(result,additionResult);
+    }
+
+    @Test
+    void getDirectionalPointer_Return_Expected_Direction() {
+        Pointer2D pointerLeft = new Pointer2D(-1,0);
+
+        assertEquals(pointerLeft, Pointer2D.getDirectionalPointer(Directions.LEFT));
     }
 
     @Test
