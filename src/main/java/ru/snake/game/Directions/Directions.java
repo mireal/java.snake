7d730cf134794cf064c4e4b01ab2e12c764f6645
mireal <moveloader@yandex.ru>
2024-07-16 13:19:27 +0300

    Added getDirectionByPointer static method to Directions class

--- src/main/java/ru/snake/game/Directions/Directions.java
+++ src/main/java/ru/snake/game/Directions/Directions.java
@@ -1,5 +1,7 @@
 package ru.snake.game.Directions;
 
+import ru.snake.game.Pointer2D.Pointer2D;
+
 public enum Directions {
     UP,
     DOWN,
@@ -21,6 +23,19 @@ public enum Directions {
                 return LEFT;
             }
             default -> throw new IllegalStateException("Unexpected value: " + this);
+        }
+    }
+
+    public static Directions getDirectionByPointer(Pointer2D pointer2D) {
+        if (Math.abs(pointer2D.x() + pointer2D.y()) != 1) {
+            return null;
+        }
+        if (pointer2D.x() == 0) {
+            if (pointer2D.y() == 1) return UP;
+            else return DOWN;
+        } else {
+            if (pointer2D.x() == 1) return RIGHT;
+            else return LEFT;
         }
     }
 }
--- src/test/java/ru/snake/game/Directions/DirectionsTest.java
+++ src/test/java/ru/snake/game/Directions/DirectionsTest.java
@@ -1,15 +1,29 @@
 package ru.snake.game.Directions;
 
 import org.junit.jupiter.api.Test;
+import ru.snake.game.Pointer2D.Pointer2D;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DirectionsTest {
 
     @Test
-    void getOpposite() {
+    void getOpposite_Return_Opposite_Direction() {
         Directions left = Directions.LEFT;
         Directions right = Directions.RIGHT;
         assertEquals(left, right.getOpposite());
+    }
+
+    @Test
+    void getDirectionByPointer_Return_Correct_Direction() {
+        Directions left = Directions.LEFT;
+        Pointer2D pointerLeft = new Pointer2D(-1,0);
+        assertEquals(left,Directions.getDirectionByPointer(pointerLeft));
+    }
+
+    @Test
+    void getDirectionByPointer_Return_Null_When_Given_Incorrect_Pointer() {
+        Pointer2D pointerLeft = new Pointer2D(-1666,124);
+        assertNull(Directions.getDirectionByPointer(pointerLeft));
     }
 }
