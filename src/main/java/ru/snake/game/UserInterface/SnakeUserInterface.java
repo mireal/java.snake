6c1733206aa013ec03971d6f4047fc7f32e65e06
mireal <moveloader@yandex.ru>
2024-07-24 11:38:41 +0300

    Added SnakeUserInterface ui interface

---
+++ src/main/java/ru/snake/game/UserInterface/SnakeUserInterface.java
@@ -0,0 +1,24 @@
+package ru.snake.game.UserInterface;
+
+import ru.snake.game.Pointer2D.Pointer2D;
+
+public interface SnakeUserInterface {
+    /**
+     * Call this function to clear the canvas.
+     */
+    void clear();
+
+    /**
+     * Passes the snake pointers array to the drawer to draw the snake body.
+     * Should be called after the clear method to avoid drawing overlay.
+     * @param snake Array of Pointer2D objects representing the snake body.
+     */
+    void drawSnake(Pointer2D[] snake);
+
+    /**
+     * Passes the fruit pointer to the drawer to draw the fruit on the canvas.
+     * Should be called after the clear method to avoid drawing overlay.
+     * @param fruit Pointer2D object containing the fruit coordinate.
+     */
+    void drawFruit(Pointer2D fruit);
+}
